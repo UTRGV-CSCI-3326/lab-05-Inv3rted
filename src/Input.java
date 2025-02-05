64cd16e67b7ef03751029b980941bd34281b3f7a
Inv3rted <84609368+Inv3rted@users.noreply.github.com>
2025-02-05 22:12:22 +0000

    Finished Assignment

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        System.out.print("Enter your weight: ");
+        float weight = input.nextFloat();
+        System.out.print("True or False: You have a history of smoking. ");
+        boolean smoker = input.nextBoolean();
+        System.out.println("Here is all of your information:");
+        System.out.println("Name: "+ name);        
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("History of smoking? " + smoker);
+    }
+}
